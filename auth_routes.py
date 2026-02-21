86850c216cb3b76d0d460db518219637f5619129
Tamires-alvesc <ttamiresacosta@hotmail.com>
2026-02-21 11:51:45 -0300

    dependencies and hash encryption

--- auth_routes.py
+++ auth_routes.py
@@ -1,10 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from models import Usuario, db
+from dependencies import pegar_sessao
+from main import bcrypt_context
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 @auth_router.get("/")
-async def autenticar():
+async def home():
     """
     Essa é a rota padrão de autenticação. Ela pode ser usada para verificar se o usuário está autenticado.
     """
     return {"mensagem": "Você acessou a rota padrão de autenticação", "autenticado": False}
+
+@auth_router.post("/criar_conta")
+async def criar_conta(email: str, senha: str, nome: str = None, session = Depends(pegar_sessao)):
+    usuario = session.query(Usuario).filter(Usuario.email == email).first()
+    if usuario:
+        return {"mensagem": "Já existe um usuário com esse email"}
+    else:
+        senha = senha[:72]  # Truncate password to 72 characters
+        senha_criptografada = bcrypt_context.hash(senha)
+        novo_usuario = Usuario(nome, email, senha_criptografada, ativo=True, admin=False)
+        session.add(novo_usuario)
+        session.commit()
+        return {"mensagem": "Conta criada com sucesso"}
